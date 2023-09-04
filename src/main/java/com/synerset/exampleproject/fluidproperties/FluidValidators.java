01a621121a6d8ec1c9117f5aeee262e94203c349
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-09-04 06:35:59 +0200

    feat(SNSUN-32): upgrade Unitility to version 1.2.0

--- src/main/java/com/synerset/exampleproject/fluidproperties/FluidValidators.java
+++ src/main/java/com/synerset/exampleproject/fluidproperties/FluidValidators.java
@@ -1,6 +1,7 @@
 package com.synerset.exampleproject.fluidproperties;
 
 import com.synerset.unitility.unitsystem.PhysicalQuantity;
+import com.synerset.unitility.unitsystem.Unit;
 import io.vavr.collection.Seq;
 import io.vavr.control.Validation;
 import org.slf4j.Logger;
@@ -12,13 +13,13 @@ public class FluidValidators {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FluidValidators.class);
 
-    public static <Q extends PhysicalQuantity<Q>> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNonNull(PhysicalQuantity<Q> physicalQuantity) {
+    public static <Q extends Unit> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNonNull(PhysicalQuantity<Q> physicalQuantity) {
         return Objects.isNull(physicalQuantity)
                 ? Validation.invalid(new InvalidQuantity("Null value passed as argument.", (PhysicalQuantity<?>) null))
                 : Validation.valid(physicalQuantity);
     }
 
-    public static <Q extends PhysicalQuantity<Q>> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNotExceedMinimalLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> minLimit) {
+    public static <Q extends Unit> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNotExceedMinimalLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> minLimit) {
         double sourceUnitInBase = physicalQuantity.toBaseUnit().getValue();
         double limitUnitInBase = minLimit.toBaseUnit().getValue();
         return sourceUnitInBase < limitUnitInBase
@@ -26,7 +27,7 @@ public class FluidValidators {
                 : Validation.valid(physicalQuantity);
     }
 
-    public static <Q extends PhysicalQuantity<Q>> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNotExceedMaximumLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> maxLimit) {
+    public static <Q extends Unit> Validation<InvalidQuantity, PhysicalQuantity<Q>> requireNotExceedMaximumLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> maxLimit) {
         double sourceUnitInBase = physicalQuantity.toBaseUnit().getValue();
         double limitUnitInBase = maxLimit.toBaseUnit().getValue();
         return sourceUnitInBase > limitUnitInBase
@@ -34,7 +35,7 @@ public class FluidValidators {
                 : Validation.valid(physicalQuantity);
     }
 
-    public static <Q extends PhysicalQuantity<Q>> Validation<InvalidQuantity, PhysicalQuantity<Q>> requirePositiveValue(PhysicalQuantity<Q> physicalQuantity) {
+    public static <Q extends Unit> Validation<InvalidQuantity, PhysicalQuantity<Q>> requirePositiveValue(PhysicalQuantity<Q> physicalQuantity) {
         double value = physicalQuantity.toBaseUnit().getValue();
         return value < 0
                 ? Validation.invalid(invalidPropertyForNegativeValue(physicalQuantity))
@@ -42,13 +43,13 @@ public class FluidValidators {
     }
 
     @SafeVarargs
-    private static <Q extends PhysicalQuantity<Q>> InvalidQuantity invalidPropertyForNull(PhysicalQuantity<Q>... inputArguments) {
+    private static <Q extends Unit> InvalidQuantity invalidPropertyForNull(PhysicalQuantity<Q>... inputArguments) {
         String message = "Null value passed as argument.";
         LOGGER.warn(message);
         return new InvalidQuantity(message, inputArguments);
     }
 
-    private static <Q extends PhysicalQuantity<Q>> InvalidQuantity invalidPropertyForExceededMinLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> minLimit) {
+    private static <Q extends Unit> InvalidQuantity invalidPropertyForExceededMinLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> minLimit) {
         String quantityName = physicalQuantity.getClass().getSimpleName();
         String message = String.format("Minimum limit exceeded: (%s)_minLimit = %s, (%s)_current = %s -> (%s)_current = %s",
                 quantityName, minLimit, quantityName, physicalQuantity.toBaseUnit(), quantityName, physicalQuantity);
@@ -56,7 +57,7 @@ public class FluidValidators {
         return new InvalidQuantity(message);
     }
 
-    private static <Q extends PhysicalQuantity<Q>> InvalidQuantity invalidPropertyForExceededMaxLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> maxLimit) {
+    private static <Q extends Unit> InvalidQuantity invalidPropertyForExceededMaxLimit(PhysicalQuantity<Q> physicalQuantity, PhysicalQuantity<Q> maxLimit) {
         String quantityName = physicalQuantity.getClass().getSimpleName();
         String message = String.format("Maximum limit exceeded: (%s)_maxLimit = %s, (%s)_current = %s -> (%s)_current = %s",
                 quantityName, maxLimit, quantityName, physicalQuantity.toBaseUnit(), quantityName, physicalQuantity);
@@ -64,7 +65,7 @@ public class FluidValidators {
         return new InvalidQuantity(message);
     }
 
-    private static <Q extends PhysicalQuantity<Q>> InvalidQuantity invalidPropertyForNegativeValue(PhysicalQuantity<Q> physicalQuantity) {
+    private static <Q extends Unit> InvalidQuantity invalidPropertyForNegativeValue(PhysicalQuantity<Q> physicalQuantity) {
         String message = "Negative value is not allowed. Current value = " + physicalQuantity;
         LOGGER.warn(message);
         return new InvalidQuantity(message);
